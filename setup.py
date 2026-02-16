7048ef487bee807451877be5b2b907fe339a7661
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-16 12:38:40 +0800

    alvistack/0.4.2
        git clean -xdf
        tar zcvf ../python-subprocess-tee_0.4.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-subprocess-tee.spec ../python-subprocess-tee_0.4.2-1.spec
        cp ../python*-subprocess-tee*0.4.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/pycontribs-subprocess-tee-0.4.2/
        rm -rf ../python*-subprocess-tee*0.4.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+"""Distribution package setuptools installer."""
+
+import site
+import sys
+
+import setuptools
+
+# See https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+
+if __name__ == "__main__":
+    setuptools.setup()
